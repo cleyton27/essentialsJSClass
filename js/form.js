e0260da7dd09c8c3de6d4592665a5a202cabd691
cleyton27 <cleyton27mb@gmail.com>
2019-01-17 07:56:43 -0300

    Otimizando o código utilizando funções que criam elementos como creatElemente(); e adicionando classes ao elemento criado utilizando classList.add() e inserindo os novos elementos criados dentro de um elemento pai com .appendChild()

--- js/form.js
+++ js/form.js
@@ -1,45 +1,24 @@
-var botaoAdcionar = document.querySelector("#adicionar-paciente");
+ var botaoAdcionar = document.querySelector("#adicionar-paciente");
 botaoAdcionar.addEventListener("click", function(event){
   event.preventDefault();
 
-  var form = document.querySelector("#form-add");
-  /*form.altura acess the element by name
-  console.log(form.altura);
-  //form.altura.value get the value of the input
-  console.log(form.altura.value);
-  console.log(form.peso.value);
-*/
+
+var form = document.querySelector("#form-add");
 
 //Extraindo informações do paciente do form
 var paciente = obterPacienteDoFormulario(form);
 console.log(paciente);
-
-//document.createElement(); creat HTML elements ex"tables"
-var pacienteTr = document.createElement("tr");
-
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
-
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calculaImc(peso, altura);
-
-//.appendChild() put a elment insed other
- pacienteTr.appendChild(nomeTd);
- pacienteTr.appendChild(pesoTd);
- pacienteTr.appendChild(alturaTd);
- pacienteTr.appendChild(gorduraTd);
- pacienteTr.appendChild(imcTd);
-
-
- console.log(pacienteTr);
+//
 //Adiconando o paciente na tabela
  var table = document.querySelector("#tabela-pacientes");
+
+
+var pacienteTr =  montarTr(paciente);
+
+
+
+
+ //console.log(pacienteTr);
 
  table.appendChild(pacienteTr);
 
@@ -52,10 +31,42 @@ function obterPacienteDoFormulario(form){
     altura: form.altura.value,
     peso: form.peso.value,
     gordura: form.gordura.value,
-    imc: calculaImc(form.peso.value, form.altura.value)
+    imc: calculaImc(form.peso.value, form.altura.value )
 
   }
 return paciente;
+}
+
+function montarTr(paciente){
+  //document.createElement(); creat HTML elements ex"tables"
+  var pacienteTr = document.createElement("tr");
+  //classList.add() add a class to element created
+  pacienteTr.classList.add("paciente");
+
+  var nomeTd = montaTd(paciente.nome, "info-nome");
+  var pesoTd = montaTd(paciente.peso, "info-peso");
+  var alturaTd = montaTd(paciente.altura, "info-altura");
+  var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+  var imcTd = montaTd(paciente.imc, "info-imc");
 
 
+
+   //.appendChild() put a elment insed other
+   pacienteTr.appendChild(nomeTd);
+   pacienteTr.appendChild(pesoTd);
+   pacienteTr.appendChild(alturaTd);
+   pacienteTr.appendChild(gorduraTd);
+   pacienteTr.appendChild(imcTd);
+
+
+  return pacienteTr;
+
+}
+
+function montaTd(dado, classe){
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+
+  return td;
 }
