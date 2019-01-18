d907d9ecfffdd8d45315f94cd7d1f76445530053
cleyton27 <cleyton27mb@gmail.com>
2019-01-18 11:25:45 -0300

    Removendo a tabela com efeitos de delay do css utilizando setTimeout(); que recebe como argumento milesegundos antes de executar uma ação

--- js/remove.js
+++ js/remove.js
@@ -1,8 +1,14 @@
-var pacientes = document.querySelectorAll(".paciente")
-console.log(pacientes);
 var tabela = document.querySelector("table");
-tabela.addEventListener("dblclick",function(){
+tabela.addEventListener("dblclick",function(event){
 //form more clean to remove HTML elements
-event.target.parentNode.remove();
+event.target.parentNode.classList.add("fadeOut");
 
-})
+//setTimeout does the wait function for milliseseconds
+
+  setTimeout(function(){
+event.target.parentNode.remove();
+},2000);
+
+
+
+});
