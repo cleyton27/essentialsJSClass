08d0a9f70209910fdb307696c84815a72d0ed45f
cleyton27 <cleyton27mb@gmail.com>
2019-01-21 09:16:15 -0300

    Implementando o search para que mostre apenas os nomes digitados no input e adiconando a classe invisble para esconder os outros nomes que não estão sendo buscados utilizando tambem 2 for um para mostrar e o outro para esconder

--- js/search.js
+++ js/search.js
@@ -8,10 +8,24 @@ console.log(campoFiltro);
 // means the same of campoFiltro.value
     console.log(this.value);
     //this for will interact by the name's array
-    var pacientes = document.querySelectorAll(".paciente");
-    for (var i = 0; i < pacientes.length; i++) {
-      var paciente = pacientes[i];
-      var tdNome = paciente.querySelector(".info-nome")
-      var nome = paciente.textContet;
+var pacientes = document.querySelectorAll(".paciente");
+    if(this.value.length >0){
+
+      for (var i = 0; i < pacientes.length; i++) {
+       var paciente = pacientes[i];
+       var tdNome = paciente.querySelector(".info-nome")
+       var nome = tdNome.textContent;
+       if(nome != this.value){
+         paciente.classList.add("invisible");
+       }else {
+         paciente.classList.remove("invisible");
+       }
+     }
+     }else{
+  for (var i = 0; i < pacientes.length; i++) {
+    var paciente= pacientes[i];
+    paciente.classList.remove("invisible");
+
+  }
     }
  });
