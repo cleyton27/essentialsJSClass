df4255b33a3919e71bba563460c819aa8fb1e104
cleyton27 <cleyton27mb@gmail.com>
2019-01-21 08:42:44 -0300

    adicionando for no arquivo search.js

--- js/search.js
+++ js/search.js
@@ -3,5 +3,15 @@ console.log(campoFiltro);
 
 //addEventListener("input", function()) get all that was typed in the field
  campoFiltro.addEventListener("input", function(){
-      console.log("digitaram algo no campo");
+   //getting the value that is being typed
+    //  console.log(campoFiltro.value);
+// means the same of campoFiltro.value
+    console.log(this.value);
+    //this for will interact by the name's array
+    var pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome")
+      var nome = paciente.textContet;
+    }
  });
