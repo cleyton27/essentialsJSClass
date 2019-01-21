6f03872e54cceef0b9bb97f74282ecc7032e017d
cleyton27 <cleyton27mb@gmail.com>
2019-01-21 08:22:12 -0300

    Adicionando o arquivo search.js e utilizando a função .addEventListener(input, function()) utilizando um input como parametro e uma função anonima para obter o que foi digitado no input

--- js/remove.js
+++ js/remove.js
@@ -3,11 +3,11 @@ tabela.addEventListener("dblclick",function(event){
 //form more clean to remove HTML elements
 event.target.parentNode.classList.add("fadeOut");
 
-//setTimeout does the wait function for milliseseconds
+//setTimeout does the function wait for milliseseconds
 
   setTimeout(function(){
 event.target.parentNode.remove();
-},2000);
+},500);
 
 
 
---
+++ js/search.js
@@ -0,0 +1,7 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+console.log(campoFiltro);
+
+//addEventListener("input", function()) get all that was typed in the field
+ campoFiltro.addEventListener("input", function(){
+      console.log("digitaram algo no campo");
+ });
